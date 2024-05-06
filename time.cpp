1640579ee37bf03d16679a19c7de5283ace12edb
mkoushan <mkoushan@proton.me>
2024-05-06 21:36:02 +0330

    Some minor improvements

--- time.cpp
+++ time.cpp
@@ -2,20 +2,27 @@
 #include <array>
 #include <algorithm>
 
+enum Period{
+    AM,
+    PM
+};
+
 class Time {
     private:
-        enum Period{
-            AM,
-            PM
-        } period;
 
         int hour, min;
+        Period period;
 
     public:
+        Time(const int& p_hour, const int& p_min) : hour(p_hour), min(p_min), period(AM) {};
         Time() : hour(12), min(0), period(AM) {
             readTime();
         };
 
+        bool operator<(const Time& other) const {
+            return this->getInMinutes() < other.getInMinutes();
+        }
+        friend std::ostream& operator<<(std::ostream& out, const Time& obj);
         void readTime() {
             int hour, min;
             char period_type[3] = "AM";
@@ -33,7 +40,7 @@ class Time {
         }
         void setMinute(const int& p_min){
             if(p_min <= 59 && p_min >= 0)
-                this->hour = p_min;
+                this->min = p_min;
             else
                 std::cout << "Invalid Argument!" << std::endl;
         }
@@ -55,11 +62,24 @@ class Time {
             }
         }
 
-        int getHour() const { return hour; }
+        int getHour() const { return this->hour; }
+        int getMinute() const { return this->min; }
+        Period getPeriod() const {return this->period;}
+        int getInMinutes() const {return this->hour * 60 + this->min + this->period * 720;}
 };
 
-int main(){
-    std::array <Time, 10> arr;
+std::ostream& operator<<(std::ostream& out, const Time& obj) {
+    out << obj.getHour() << ' ' << obj.getMinute() << ' ' << (obj.getPeriod() == AM ? "AM" : "PM");
+    return out;
+}
 
+int main(){
+    Time m(0, 0);
+    std::array <Time, 10> arr;
+    
+    for (auto& k : arr) 
+        m = std::max(m, k);
+
+    std::clog << m;
     return 0; 
 } 
